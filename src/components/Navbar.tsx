acae4f803ed3f5e475109a61e907cf63e96945a3
Ashik K L <ashikkl19112001@gmail.com>
2023-08-14 23:43:37 +0530

    add: logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { ThemeToggle } from "./ThemeToggle";
 import { Button } from "./ui/Button";
 import Link from "next/link";
@@ -8,17 +9,17 @@ function Navbar() {
   return (
     <div className="  left-0 right-0 top-0 z-50 flex  h-20 items-center justify-between shadow-sm">
       <div className="container mx-auto flex w-full max-w-7xl items-center justify-between">
-        <div className=" gap-4 flex items-center">
+        <div className=" hidden gap-4 md:flex items-center justify-center">
           <Link href="/">
             <div className="flex flex-row gap-1 items-center">
-              {/* <Image
-                className="relative z-0 h-8 w-auto"
-                src="/logo.png"
+              <Image
+                className="relative z-0 h-8 w-auto  saturate-[57.27] hue-rotate-[359deg] "
+                src="/logo.svg"
                 alt="Logo"
                 priority={true}
                 width={512}
                 height={512}
-              /> */}
+              />
               <div className="red_gradient text relative text-[1rem] font-extrabold leading-relaxed">
                 The Thieves&apos; Library
               </div>
@@ -27,17 +28,18 @@ function Navbar() {
           <ThemeToggle />
         </div>
         {/* For mobile view */}
-        <div className="flex flex-row gap-2 md:hidden items-center">
+        <div className="flex flex-row gap-2 md:hidden items-center justify-center">
           <Link href="/">
-            {/* <Image
-              className="relative z-0 h-8 w-auto"
-              src="/logo.png"
+            <Image
+              className="relative z-0 h-8 w-auto invert dark:invert-0 opacity-90 grayscale"
+              src="/logo.svg"
               alt="Logo"
               priority={true}
               width={512}
               height={512}
-            /> */}
+            />
           </Link>
+          <ThemeToggle />
         </div>
       </div>
     </div>
